6af9094da7a0cce9d33a13479b4da66b453a86a9
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-18 20:53:34 -0300

    Apostila04add18.03

---
+++ Apostila04/src/br/com/fiap/loja/model/Produto.java
@@ -0,0 +1,8 @@
+package br.com.fiap.loja.model;
+
+public class Produto {
+
+    double preco;
+    int quantidade;
+
+}
---
+++ Apostila04/src/br/com/fiap/loja/view/Terminal.java
@@ -0,0 +1,12 @@
+package br.com.fiap.loja.view;
+
+public class Terminal {
+
+    public static void main(String[] args) {
+        //Ler os dados do produto
+        //Criar o objeto do produto
+        //Atribuir os valores ao objeto
+
+        //Exibir os valores dos atributos
+    }
+}
